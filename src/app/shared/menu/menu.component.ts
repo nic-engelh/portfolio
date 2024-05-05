6470717e500297962ffae42b13be8e9140c5c4bb
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-05-05 16:46:33 +0200

    added slide animation fixes

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
+  animations: [
+    trigger('slideAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateY(100vh)', opacity: 0 }),
+        animate('500ms ease-in-out', style({ transform: 'translateY(0)', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('500ms ease-in-out', style({ transform: 'translateY(-100vh)', opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class MenuComponent {
+
+
+  showMenu = false;
+  animationState = 'enter';
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  ngOnInit() {
+    // Set animationState to 'enter' initially
+    this.animationState = 'enter';
+  }
+
+  ngOnDestroy() {
+    // Set animationState to 'leave' when the component is destroyed
+    this.animationState = 'leave';
+  }
 
 }
