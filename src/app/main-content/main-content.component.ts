cf486b43c08996aa68308721a95f38776884baae
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-05-11 12:30:10 +0200

    added landing page html content

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { Router } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, LandingPageComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
