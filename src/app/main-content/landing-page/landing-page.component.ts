a38438556535de0ab30f3b0fde1eb184d3e24336
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-05-18 15:08:50 +0200

    Added arrow animation

--- src/app/main-content/landing-page/landing-page.component.ts
+++ src/app/main-content/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+
+  imagesArrow = [
+    '../../../assets/img/animation/arrow/arrow_1.svg',
+    '../../../assets/img/animation/arrow/arrow_2.svg',
+    '../../../assets/img/animation/arrow/arrow_3.svg',
+    '../../../assets/img/animation/arrow/arrow_4.svg',
+    '../../../assets/img/animation/arrow/arrow_5.svg'
+  ];
+
+  currentImage: string = '';
+  animationIntervalId: any;
+  currentImageIndex = 0;
+
+  constructor () { }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.showArrowAnimation();
+  }
+
+  showArrowAnimation () {
+      this.animationIntervalId = setInterval(() => {
+        this.currentImageIndex = (this.currentImageIndex + 1) % this.imagesArrow.length;
+    }, 200);
+  }
 
 }
