bf25e74f7d38cfce75ba0ed399e741d11af9aba1
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-05-18 19:32:10 +0200

    Updated animated arrrow

--- src/app/main-content/landing-page/landing-page.component.ts
+++ src/app/main-content/landing-page/landing-page.component.ts
@@ -1,38 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { interval } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
+import { setInterval } from 'timers';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent implements OnInit {
-
-  imagesArrow = [
-    '../../../assets/img/animation/arrow/arrow_1.svg',
-    '../../../assets/img/animation/arrow/arrow_2.svg',
-    '../../../assets/img/animation/arrow/arrow_3.svg',
-    '../../../assets/img/animation/arrow/arrow_4.svg',
-    '../../../assets/img/animation/arrow/arrow_5.svg'
+export class LandingPageComponent implements OnInit{
+  arrowImages = [
+    'assets/img/animation/arrow/arrow_1.svg',
+    'assets/img/animation/arrow/arrow_2.svg',
+    'assets/img/animation/arrow/arrow_3.svg',
+    'assets/img/animation/arrow/arrow_4.svg',
+    'assets/img/animation/arrow/arrow_5.svg',
   ];
 
-  currentImage: string = '';
-  animationIntervalId: any;
-  currentImageIndex = 0;
+  currentImageIndex = 1;
 
-  constructor () { }
+  componentActive = true;
+
+  intervalId: any;
+
+  constructor(){ }
+
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.showArrowAnimation();
+    this.startAnimation();
   }
 
-  showArrowAnimation () {
-      this.animationIntervalId = setInterval(() => {
-        this.currentImageIndex = (this.currentImageIndex + 1) % this.imagesArrow.length;
-    }, 200);
+  startAnimation () {
+    setInterval(() => {
+        this.currentImageIndex = (this.currentImageIndex + 1) % this.arrowImages.length;
+      }, 500);
   }
 
+  ngOnDestroy() {
+  }
 }
