221b2438e58b70213c42cf4d8ea6e868bd379b54
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-05-19 19:22:40 +0200

    added new images and first about me structure

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, NgZone } from '@angular/core';
+import { clearInterval } from 'node:timers';
 
 @Component({
   selector: 'app-about-me',
@@ -7,6 +8,41 @@ import { Component } from '@angular/core';
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements OnInit, OnDestroy {
+
+  arrowImages = [
+    'src/assets/img/animation/arrow-left/arrow-left-1.svg',
+    'src/assets/img/animation/arrow-left/arrow-left-2.svg',
+    'src/assets/img/animation/arrow-left/arrow-left-3.svg'
+  ]
+  currentImageIndex: number = 0;
+  intervalId: any;
+
+  constructor (private ngZone: NgZone) { };
+
+  ngOnInit(): void {
+    this.startAnimation();
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    clearInterval(this.intervalId);
+  }
+
+  startAnimation () {
+    this.ngZone.runOutsideAngular(() => {
+      this.intervalId = setInterval(() => {
+        this.ngZone.run(() => {
+          this.currentImageIndex =
+            (this.currentImageIndex + 1) % this.arrowImages.length;
+        });
+      }, 350);
+    });
+
+
+  }
+
+
 
 }
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { Router } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [NavbarComponent, LandingPageComponent],
+  imports: [NavbarComponent, LandingPageComponent, AboutMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
