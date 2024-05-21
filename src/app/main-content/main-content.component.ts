3f03bd02faa3f451a9ddb621160eb57acefbf783
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-05-21 21:56:22 +0200

    added contact html and css

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { Router } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [NavbarComponent, LandingPageComponent, AboutMeComponent],
+  imports: [NavbarComponent, LandingPageComponent, AboutMeComponent, ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
