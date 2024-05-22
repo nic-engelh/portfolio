fa537a95959471cec6d19339d307c2ef2fe3f1ba
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-05-22 13:41:09 +0200

    added input form to contact section

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      fullname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.invalid) {
+      this.contactForm.markAllAsTouched();
+    } else {
+      // Handle form submission
+      console.log('Form submitted:', this.contactForm.value);
+    }
+  }
 }
