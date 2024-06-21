c01c5f7566660d28847572e20efa6293034d8f46
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-06-21 10:18:52 +0200

    added comp to portfolio and added animation syntax

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
   imports: [],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrl: './portfolio.component.scss',
+  animations:[
+    trigger('slideAnimation', [
+      state('hidden', style({
+        transform: 'translateX(-100%)'
+      })),
+      state('visible', style({
+        transform: 'translateX(0)'
+      })),
+      transition('hidden <=> visible', [
+        animate('0.3s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class PortfolioComponent {
+  isHovered = false;
+
+  projects: {name: string, imagePath: string}[] = [
+    {name: "Pokedex", imagePath: "/assets/img/"},
+    {name: "Join", imagePath: "/assets/img/"},
+    {name: "Sharky", imagePath: "/assets/img/"},
+  ]
+
+  onHover() {
+    this.isHovered = true;
+  }
+
+  onLeave() {
+    this.isHovered = false;
+  }
+
+  getProjectNumber () {
+    return this.projects.length;
+  }
+
+
 
 }
