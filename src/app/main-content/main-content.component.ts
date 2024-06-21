5d2a05f2ccc570e2dab172ca4efa7bdf191e325d
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-06-21 11:54:53 +0200

    added more to portfolio

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -5,11 +5,12 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ContactComponent } from './contact/contact.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [NavbarComponent, LandingPageComponent, AboutMeComponent, ContactComponent, MySkillsComponent],
+  imports: [NavbarComponent, LandingPageComponent, AboutMeComponent, ContactComponent, MySkillsComponent, PortfolioComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
