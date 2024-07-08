263e8510ee8c4b8f59ac0373bc2a1b42d95928c3
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-07-08 16:33:37 +0200

    added ngx translation package and added syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,11 @@ import { FooterComponent } from './shared/footer/footer.component';
 })
 export class AppComponent{
   title = 'portfolio';
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('de');
+  }
 }
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,30 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  bootstrapApplication(AppComponent, {
+    providers: [
+      importProvidersFrom(
+        HttpClientModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          },
+          defaultLanguage: 'en'
+        })
+      )
+    ]
+  });
