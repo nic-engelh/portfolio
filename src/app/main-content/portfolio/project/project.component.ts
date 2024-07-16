457f31417812f4edea236735bdaa52a8528966e5
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-07-16 18:13:33 +0200

    Added links

--- src/app/main-content/portfolio/project/project.component.ts
+++ src/app/main-content/portfolio/project/project.component.ts
@@ -11,10 +11,10 @@ import { TranslateService,TranslateModule} from '@ngx-translate/core';
 export class ProjectComponent {
   @Input() projectName?: string;
 
-  projects: {name: string, stack: string, info: string}[] = [
-    {name: "Pokedex", stack: "HTML | CSS | JavaScript | API", info:"PORTFOLIO-POKEDEX"},
-    {name: "Join", stack: "HTML | CSS | JavaScript", info:"PORTFOLIO-JOIN"},
-    {name: "Sharky", stack: "HTML | CSS | JavaScript", info:"PORTFOLIO-SHARKY"},
+  projects: {name: string, stack: string, info: string, href: string, github: string}[] = [
+    {name: "Pokedex", stack: "HTML | CSS | JavaScript | API", info:"PORTFOLIO-POKEDEX", href:"https://www.niklas-engelhardt.de/pokedex", github:"https://github.com/nic-engelh/pokedex"},
+    {name: "Join", stack: "HTML | CSS | JavaScript", info:"PORTFOLIO-JOIN", href:"https://www.niklas-engelhardt.de/join", github:"https://github.com/nic-engelh/join"},
+    {name: "Sharky", stack: "HTML | CSS | JavaScript", info:"PORTFOLIO-SHARKY", href:"https://www.niklas-engelhardt.de/sharky", github:"https://github.com/nic-engelh/sharky"},
   ]
 
   constructor (public translate: TranslateService) {
