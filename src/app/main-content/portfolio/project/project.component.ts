013469cc7f66c1fd25b7ed263d224c54338e8bdc
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-07-17 15:17:40 +0200

    added link to projects

--- src/app/main-content/portfolio/project/project.component.ts
+++ src/app/main-content/portfolio/project/project.component.ts
@@ -12,7 +12,7 @@ export class ProjectComponent {
   @Input() projectName?: string;
 
   projects: {name: string, stack: string, info: string, href: string, github: string}[] = [
-    {name: "Pokedex", stack: "HTML | CSS | JavaScript | API", info:"PORTFOLIO-POKEDEX", href:"https://www.niklas-engelhardt.de/pokedex", github:"https://github.com/nic-engelh/pokedex"},
+    {name: "Pokedex", stack: "HTML | CSS | JavaScript | API | Bootstrap", info:"PORTFOLIO-POKEDEX", href:"https://www.niklas-engelhardt.de/pokedex", github:"https://github.com/nic-engelh/pokedex"},
     {name: "Join", stack: "HTML | CSS | JavaScript", info:"PORTFOLIO-JOIN", href:"https://www.niklas-engelhardt.de/join", github:"https://github.com/nic-engelh/join"},
     {name: "Sharky", stack: "HTML | CSS | JavaScript", info:"PORTFOLIO-SHARKY", href:"https://www.niklas-engelhardt.de/sharky", github:"https://github.com/nic-engelh/sharky"},
   ]
