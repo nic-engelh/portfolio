845238787c211ad75d28243bbd4ec0003d8c9595
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-07-22 12:06:11 +0200

    Fixed CSS and added Routerlink to menu

--- src/app/shared/navbar/menu/menu.component.ts
+++ src/app/shared/navbar/menu/menu.component.ts
@@ -1,8 +1,9 @@
-import { Component, input, output } from '@angular/core';
+import { Component, Inject, output, OnInit } from '@angular/core';
 import { CommonModule} from '@angular/common';
+import { DOCUMENT } from '@angular/common';
 import { NgxPageScrollModule } from 'ngx-page-scroll'
 import { TranslateService,TranslateModule} from '@ngx-translate/core';
-
+import { Router } from '@angular/router';
 
 
 const hidden = {transform: 'translateY(100%)'};
@@ -20,17 +21,37 @@ const timing = '150ms ease-in-out';
 
 })
 export class MenuComponent {
-
+  isImprintActive: boolean = false;
+  public href: string = "";
   onMenuChange = output<boolean>();
 
-  constructor (public translate: TranslateService) {
+  constructor (public translate: TranslateService, private router: Router) {
     this.translate.setDefaultLang('en');
   }
 
-  updateMenu(menuOpen: boolean) {
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.href = this.router.url;
+    this.checkRoute();
+  }
+
+  async updateMenu(menuOpen: boolean) {
+    if (this.isImprintActive) {
+     this.goToHomeRoute();
+    }
     setTimeout(() => {
       this.onMenuChange.emit(menuOpen);
     }, 1800);
   }
 
+  goToHomeRoute() {
+    this.router.navigate(['/']);
+  }
+
+  checkRoute() {
+    if (this.href === "/imprint") {
+      this.isImprintActive = true;
+    }
+  }
 }
